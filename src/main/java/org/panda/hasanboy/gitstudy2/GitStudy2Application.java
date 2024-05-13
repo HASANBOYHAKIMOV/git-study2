876d85dc13077dcf175120c132ca1fe25bb6a19c
hasanboy <hasanboyhakimov95@gmail.com>
2024-05-13 19:12:29 +0500

    Added foo to the bar

--- src/main/java/org/panda/hasanboy/gitstudy2/GitStudy2Application.java
+++ src/main/java/org/panda/hasanboy/gitstudy2/GitStudy2Application.java
@@ -1,5 +1,6 @@
 package org.panda.hasanboy.gitstudy2;
 
+import ch.qos.logback.core.net.SyslogOutputStream;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class GitStudy2Application {
 
     public static void main(String[] args) {
+        System.out.println("new line");
+        System.out.println("hello");
+        System.out.println("world");
+        System.out.println("hello world");
+        System.out.println("hello world");
+        System.out.println("Hasanboy Hakimov");
         SpringApplication.run(GitStudy2Application.class, args);
     }
 
