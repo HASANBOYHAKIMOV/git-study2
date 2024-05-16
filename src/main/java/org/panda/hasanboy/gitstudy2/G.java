f1d13bc67ac58d510f18a7dac29cb5bf74715e1a
hasanboy <hasanboyhakimov95@gmail.com>
2024-05-16 06:49:15 +0500

    a

--- src/main/java/org/panda/hasanboy/gitstudy2/G.java
+++ src/main/java/org/panda/hasanboy/gitstudy2/G.java
@@ -2,4 +2,11 @@ package org.panda.hasanboy.gitstudy2;
 
 public class G {
     // line delete
+    public void methodB(){
+        System.out.println("B");
+        System.out.println("B");
+
+        System.out.println("rebaseMethod_B_1");
+        System.out.println("rebaseMethod_B_2");
+    }
 }
